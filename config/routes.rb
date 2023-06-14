c54ab27717a57ffabbf9cd733611d09520d3bff4
Progress2002 <ezeamakachisom2002@gmail.com>
2023-06-14 15:36:26 +0100

    Add  logic for displaying  posts and users  view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users, only: [:index, :show] do
+  get "/", to: "users#index"
+  resources :users, only: [ :show] do
     resources :posts, only: [:index, :show]
   end
 end
