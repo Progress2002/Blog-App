e5fcb6391b521f8c4532c5999440681e781157c2
Progress2002 <ezeamakachisom2002@gmail.com>
2023-06-15 14:58:07 +0100

    Refactor codes and fix lintererrors

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -19,15 +19,15 @@ module UsersHelper
 
   def render_post(user, post)
     link_to(user_post_path(user, post)) do
-      content_tag(:div, class: "post-container") do
-        concat(content_tag(:h3, post.title, class: "post-title"))
-        concat(content_tag(:div, class: "post-details") do
-          concat(content_tag(:p, "#{post.text[0..100]}...", class: "details"))
+      content_tag(:div, class: 'post-container') do
+        concat(content_tag(:h3, post.title, class: 'post-title'))
+        concat(content_tag(:div, class: 'post-details') do
+          content_tag(:p, truncate(post.text, length: 100, omission: '...'), class: 'details')
         end)
-        concat(content_tag(:div, class: "comment-like-container") do
-          concat(content_tag(:p, class: "coment-likes-data") do
+        concat(content_tag(:div, class: 'comment-like-container') do
+          content_tag(:p, class: 'coment-likes-data') do
             "Comment: #{post.comment_counter || 0}, Likes #{post.likes_counter || 0}"
-          end)
+          end
         end)
       end
     end
