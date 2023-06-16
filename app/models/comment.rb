a66053dc95099bbc144ba54192044350d093f69b
Progress2002 <ezeamakachisom2002@gmail.com>
2023-06-16 13:31:41 +0100

    Create comment controller and add functionaliti

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user, foreign_key: 'user_id'
   belongs_to :post
-  after_save :update_comment_counter
+  after_create :update_comment_counter
 
   def update_comment_counter
     post.update(comment_counter: post.comments.count)
