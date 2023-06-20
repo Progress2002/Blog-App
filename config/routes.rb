68e6de791d0b7c8d330ac330845ee3bbb50435e0
Progress2002 <ezeamakachisom2002@gmail.com>
2023-06-20 20:41:28 +0100

    Setup devise gem to handle users autentication and sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'sign_up', to: 'devise/registrations#new'
+  end
+
+  devise_for :users, skip: :all
   root "users#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :new, :show] do
