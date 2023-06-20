1ef69fca3945960778860fbd2fe48e1d0bba2c93
Progress2002 <ezeamakachisom2002@gmail.com>
2023-06-20 20:43:23 +0100

    Update controllers

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,13 @@
-class LikesController < ApplicationController
-  def create
-    @user = User.find(params[:user_id])
-    @likes = Like.create(user_id: @user.id, post_id: params[:post_id])
-    redirect_to request.path
-  end
-end
+# class LikesController < ApplicationController
+#   def create
+#     @user = User.find(params[:user_id])
+#     if @user.user_has_liked(params[:post_id])
+#       @user.remove_user_like(params[:post_id])
+#     else
+#       @user.add_user_like(params[:post_id])
+#     end
+#     # @likes = Like.create(user_id: @user.id, post_id: params[:post_id])
+#     redirect_to request.path
+#   end
+
+# end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
-class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-    @posts = @user.recent_post
-  end
+# class UsersController < ApplicationController
+#   before_action :authenticate_user!
+#   def show
+#     @user = User.includes(:posts).find(params[:id])
+#     @posts = @user.recent_post
+#     @post_counter = @user.posts.length
+#   end
 
-  def index
-    @users = User.all
-  end
-end
+#   def index
+#     @users = User.all
+#   end
+# end
