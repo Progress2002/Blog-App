76ba80d04274134848c8dd7403761c7b9786cac1
Progress2002 <ezeamakachisom2002@gmail.com>
2023-06-21 02:23:30 +0100

    Update likes funtionality

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,14 @@
 class LikesController < ApplicationController
   def create
     @user = User.find(params[:user_id])
-    if @user.user_has_liked(params[:post_id])
-      @user.remove_user_like(params[:post_id])
+    @post = Post.find(params[:post_id])
+
+    if @user.user_has_liked?(@post, @user )
+      @user.remove_user_like(@post, @user )
+      puts 'Unlike'
     else
-      @user.add_user_like(params[:post_id])
+      @user.add_user_like(@post, @user )
+      puts 'Like'
     end
     # @likes = Like.create(user_id: @user.id, post_id: params[:post_id])
     redirect_to request.path
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,21 +8,20 @@ class User < ApplicationRecord
   has_many :likes
 
   validates :name, presence: true
-  validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
   def recent_post
     posts.order(created_at: :desc).limit(3)
   end
 
-  def user_has_liked(post_id)
-    likes.exists?(post_id: post_id)
+  def user_has_liked?(post, user)
+    likes.exists?(post: post, user: user)
   end
 
-  def remove_user_like(post_id)
-    likes.find_by(post_id: post_id).destroy
+  def remove_user_like(post, user)
+    likes.find_by(post: post, user: user).destroy
   end
 
-  def add_user_like(post_id)
-    likes.create(post_id: post_id)
+  def add_user_like(post, user )
+    likes.create(post: post, user: user)
   end
 end
