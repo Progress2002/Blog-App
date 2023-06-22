7cc56e9ffe7cb084ff5ceac8dc76d8716b302e69
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-06-22 06:42:02 +0000

    implemented admin roles in post and comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,20 +1,20 @@
-# class Post < ApplicationRecord
-#   belongs_to :user, foreign_key: 'user_id'
-#   has_many :comments, dependent: :destroy
-#   has_many :likes, dependent: :destroy
+class Post < ApplicationRecord
+  belongs_to :user, foreign_key: 'user_id'
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
-#   validates :title, presence: true, length: { minimum: 3, maximum: 250 }
-#   validates :comment_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
-#   validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
-#   validates :text, presence: true, length: { minimum: 3, maximum: 250 }
-#   after_create :post_count_updater
-#   after_destroy :post_count_updater
+  validates :title, presence: true, length: { minimum: 3, maximum: 250 }
+  validates :comment_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :text, presence: true, length: { minimum: 3, maximum: 250 }
+  after_create :post_count_updater
+  after_destroy :post_count_updater
 
-#   def post_count_updater
-#     user.update(posts_counter: user.posts.count)
-#   end
+  def post_count_updater
+    user.update(posts_counter: user.posts.count)
+  end
 
-#   def recent_comments
-#     comments.includes(:user).order(created_at: :desc).limit(5)
-#   end
-# end
+  def recent_comments
+    comments.includes(:user).order(created_at: :desc).limit(5)
+  end
+end
