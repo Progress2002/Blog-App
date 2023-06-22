91dc0b7b90327b30f5e469c0aea719f7cbfea3ff
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-06-22 06:41:17 +0000

    create functionality to delete post and comment

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,19 +1,19 @@
-# # frozen_string_literal: true
+# frozen_string_literal: true
 
-# class Ability
-#   include CanCan::Ability
+class Ability
+  include CanCan::Ability
 
-#   def initialize(user)
+  def initialize(user)
 
-#     user ||= User.new # guest user (not logged in)
+    user ||= User.new # guest user (not logged in)
 
-#     # Define abilities for the different user roles
-#     if user.role == 'admin'
-#       can :destroy, [Post, Comment]
-#     else
-#       can :destroy, Post, user_id: user.id
-#       can :destroy, Comment, user_id: user.id
-#     end
+    # Define abilities for the different user roles
+    if user.role == 'admin'
+      can :destroy, [Post, Comment]
+    else
+      can :destroy, Post, user_id: user.id
+      can :destroy, Comment, user_id: user.id
+    end
     
-#   end
-# end
+  end
+end
