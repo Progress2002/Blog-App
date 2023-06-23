79961f24e8fd9ca931d99d85331b0a560470151a
Progress2002 <ezeamakachisom2002@gmail.com>
2023-06-23 01:33:49 +0100

    Setup API endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+
+  namespace :api do
+    get '/users/:user_id/posts', to: 'posts#index'
+    post '/comments', to: 'comments#create'
+    get '/users/:user_id/posts/:post_id/comments', to: 'comments#index'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
